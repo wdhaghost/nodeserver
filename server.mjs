6e15d5dec52f0ea29ca345f73083ae193ef18586
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-05 10:10:07 +0100

    init

---
+++ server.mjs
@@ -0,0 +1,13 @@
+
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3001');
+});
+
