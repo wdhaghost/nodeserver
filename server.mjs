d1718994c24654954043a25b1641c89bb260835d
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-12-05 10:11:56 +0100

    port 3001

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ const server = createServer((req, res) => {
 });
 
 
-server.listen(3000, '127.0.0.1', () => {
+server.listen(3001, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3001');
 });
 
